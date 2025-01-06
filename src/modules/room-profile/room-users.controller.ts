2ef3e874c37dd04f5bfb366abfdd005ddf48124d
Dostonbek O'ktamov <rustamdostondev@gmail.com>
2025-01-06 15:47:12 +0500

    add: fix some issue

--- src/modules/room-profile/room-users.controller.ts
+++ src/modules/room-profile/room-users.controller.ts
@@ -1,14 +1,13 @@
 import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
 import { CreateRoomUserDto } from './dto/create-room-user.dto';
-import { RoomProfile } from '@prisma/client';
 import {
   ApiTags,
   ApiOperation,
-  ApiResponse,
   ApiParam,
   ApiBearerAuth,
 } from '@nestjs/swagger';
 import { RoomProfilesService } from './room-users.service';
+import { User } from '@common/decorators/user.decorator';
 
 @ApiTags('room-profiles')
 @Controller('room-profiles')
@@ -16,44 +15,32 @@ import { RoomProfilesService } from './room-users.service';
 export class RoomProfilesController {
   constructor(private readonly roomProfilesService: RoomProfilesService) {}
 
-  @Post()
+  @Post('create')
   @ApiOperation({ summary: 'Create a new room profile' })
-  @ApiResponse({
-    status: 201,
-    description: 'Room profile has been successfully created.',
-  })
-  @ApiResponse({ status: 400, description: 'Bad Request.' })
   async createRoomProfile(
     @Body() createRoomUserDto: CreateRoomUserDto,
-  ): Promise<RoomProfile> {
-    return this.roomProfilesService.createRoomProfile(createRoomUserDto);
+    @User() user: any,
+  ) {
+    return this.roomProfilesService.createRoomProfile(createRoomUserDto, user);
   }
 
-  @Get()
+  @Get('getAll')
   @ApiOperation({ summary: 'Get all room profiles' })
-  @ApiResponse({ status: 200, description: 'Return all room profiles.' })
-  async getRoomProfiles(): Promise<RoomProfile[]> {
+  async getRoomProfiles() {
     return this.roomProfilesService.getRoomProfiles();
   }
 
-  @Get(':id')
+  @Get('getById/:id')
   @ApiOperation({ summary: 'Get a room profile by id' })
   @ApiParam({ name: 'id', description: 'Room Profile ID' })
-  @ApiResponse({ status: 200, description: 'Return the room profile.' })
-  @ApiResponse({ status: 404, description: 'Room profile not found.' })
-  async getRoomProfileById(@Param('id') id: string): Promise<RoomProfile> {
+  async getRoomProfileById(@Param('id') id: string) {
     return this.roomProfilesService.getRoomProfileById(id);
   }
 
-  @Delete(':id')
+  @Delete('deletedById/:id')
   @ApiOperation({ summary: 'Delete a room profile' })
   @ApiParam({ name: 'id', description: 'Room Profile ID' })
-  @ApiResponse({
-    status: 200,
-    description: 'Room profile has been successfully deleted.',
-  })
-  @ApiResponse({ status: 404, description: 'Room profile not found.' })
-  async deleteRoomProfile(@Param('id') id: string): Promise<RoomProfile> {
-    return this.roomProfilesService.deleteRoomProfile(id);
+  async deleteRoomProfile(@Param('id') id: string, @User() user: any) {
+    return this.roomProfilesService.deleteRoomProfile(id, user);
   }
 }
--- src/modules/room-profile/room-users.service.ts
+++ src/modules/room-profile/room-users.service.ts
@@ -1,42 +1,55 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { RoomProfile } from '@prisma/client';
 import { PrismaService } from '@modules/prisma/prisma.service';
 import { CreateRoomUserDto } from './dto/create-room-user.dto';
 import { objectId } from '@utils';
+import { CustomApiResponse } from '@common/utils/api-response.util';
+import { IApiResponse } from '@common/interfaces/api-response.interface';
 
 @Injectable()
 export class RoomProfilesService {
   constructor(private readonly prisma: PrismaService) {}
 
-  async getRoomProfiles(): Promise<RoomProfile[]> {
-    return this.prisma.roomProfile.findMany({
+  async getRoomProfiles(): Promise<IApiResponse<RoomProfile[]>> {
+    const exit = await this.prisma.roomProfile.findMany({
       include: {
         profile: true,
         room: true,
         messages: true,
       },
     });
+    return CustomApiResponse.success(exit);
   }
 
-  async getRoomProfileById(id: string): Promise<RoomProfile> {
-    return this.prisma.roomProfile.findFirst({
-      where: { id },
+  async getRoomProfileById(id: string): Promise<IApiResponse<RoomProfile>> {
+    const exit = await this.prisma.roomProfile.findFirst({
+      where: { id, isDeleted: false },
       include: {
         profile: true,
         room: true,
         messages: true,
       },
     });
+
+    if (!exit) {
+      throw new NotFoundException(`RoomProfile with ID ${id} not found`);
+    }
+
+    return CustomApiResponse.success(exit);
   }
 
   async createRoomProfile(
     createRoomUserDto: CreateRoomUserDto,
-  ): Promise<RoomProfile> {
-    return this.prisma.roomProfile.create({
+    user: any,
+  ): Promise<IApiResponse<RoomProfile>> {
+    const data = await this.prisma.roomProfile.create({
       data: {
         id: objectId(),
         profileId: createRoomUserDto.profileId,
         roomId: createRoomUserDto.roomId,
+
+        createdAt: new Date(),
+        createdBy: user.id,
       },
       include: {
         profile: true,
@@ -44,16 +57,36 @@ export class RoomProfilesService {
         messages: true,
       },
     });
+
+    return CustomApiResponse.success(data);
   }
 
-  async deleteRoomProfile(id: string): Promise<RoomProfile> {
-    return this.prisma.roomProfile.delete({
+  async deleteRoomProfile(
+    id: string,
+    user: any,
+  ): Promise<IApiResponse<RoomProfile>> {
+    const exist = await this.prisma.roomProfile.findFirst({
+      where: { id, isDeleted: false },
+    });
+
+    if (!exist) {
+      throw new NotFoundException(`RoomProfile with ID ${id} not found`);
+    }
+
+    const data = await this.prisma.roomProfile.update({
       where: { id },
+      data: {
+        isDeleted: true,
+        deletedAt: new Date(),
+        deletedBy: user.id,
+      },
       include: {
         profile: true,
         room: true,
         messages: true,
       },
     });
+
+    return CustomApiResponse.success(data);
   }
 }
